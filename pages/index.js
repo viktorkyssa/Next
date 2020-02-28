d173ef4f2fa39e650b5d520481c199f5d68edde6
Viktor <viktor.kyssa@cation.ee>
2020-02-28 18:42:16 +0200

    First commit - started to learn next by their doc

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import Layout from '../components/MyLayout';
+
+const PostLink = props => (
+    <li>
+        <Link href="/p/[id]" as={`/p/${props.id}`}>
+            <a>{props.id}</a>
+        </Link>
+    </li>
+);
+
+export default function Blog() {
+    return (
+        <Layout>
+            <h1>My Blog</h1>
+            <ul>
+                <PostLink id="hello-nextjs" />
+                <PostLink id="learn-nextjs" />
+                <PostLink id="deploy-nextjs" />
+            </ul>
+        </Layout>
+    );
+}
